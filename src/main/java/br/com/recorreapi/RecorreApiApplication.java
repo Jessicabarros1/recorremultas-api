2cca8b16a93317c842ce2c90cf214f4495ec20e7
aluno <aluno@ifpe.com>
2023-04-15 15:28:45 -0300

    pp2 primeiraparte

---
+++ src/main/java/br/com/recorreapi/RecorreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.recorreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecorreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecorreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/recorreapi/RecorreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.recorreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecorreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
